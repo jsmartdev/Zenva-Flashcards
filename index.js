8e7055cd5028252d4fdd61bf3a1f9d8f00851ea4
Jesse Smart <jsmartdev@protonmail.com>
2021-03-03 17:38:41 -0500

    Complete Implement Show/Hide For All Cards

---
+++ index.js
@@ -0,0 +1,37 @@
+/* eslint-disable */
+
+let firstButton = document.getElementsByClassName("card-button")[0];
+
+let firstSpanish = document.getElementsByClassName("card-spanish")[0];
+
+firstButton.addEventListener("click", function(event) {
+    firstSpanish.style.display = "block";
+    
+});
+
+let secondButton = document.getElementsByClassName("card-button")[1];
+
+let secondSpanish = document.getElementsByClassName("card-spanish")[1];
+
+secondButton.addEventListener("click", function(event) {
+    secondSpanish.style.display = "block";
+    
+});
+
+let thirdButton = document.getElementsByClassName("card-button")[2];
+
+let thirdSpanish = document.getElementsByClassName("card-spanish")[2];
+
+thirdButton.addEventListener("click", function(event) {
+    thirdSpanish.style.display = "block";
+    
+});
+
+let fourthButton = document.getElementsByClassName("card-button")[3];
+
+let fourthSpanish = document.getElementsByClassName("card-spanish")[3];
+
+fourthButton.addEventListener("click", function(event) {
+    fourthSpanish.style.display = "block";
+    
+});
